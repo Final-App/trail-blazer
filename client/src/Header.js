f203e3b09d6306e5951293c79283a9613c802fa1
Danny Tomkins <tomkinsdan@gmail.com>
2021-03-21 10:46:25 -0500

    added brand img

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import BrewHopperImg from './img/brew-hopper-brand.png'
 
 function Header(){
     return(
         <div className="app-header">
+        <img src={BrewHopperImg}/>
         </div>
     );
 }
